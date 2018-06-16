9a9e40350b06f207ec61bfe8386fb4dd82f9c2fe
Robert Reihs <robert.reihs@medunigraz.at>
2018-06-16 15:09:42 +0200

    Start Updating Landing Page
    Signed-off-by: Robert Reihs <robert.reihs@medunigraz.at>

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function indexHomeAction(LoggerInterface $logger)
+    {
+        $logger->info("DefaultController!");
+        return $this->render('default/main.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
     /**
      * @Route("/default", name="default")
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
         $logger->info("DefaultController!");
         return $this->render('default/index.html.twig', [
