3a5f45fe37f9aa1c3ebf58ffa895b8e8303f3d79
Robert Reihs <robert.reihs@medunigraz.at>
2019-01-01 20:55:47 +0100

    Update to handle changes for reservations

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -117,6 +117,88 @@ class DashboardController extends Controller
             }
         }
 
+        // Edit Data
+        if ($form->isSubmitted() && $form->isValid() && $form->getClickedButton() && 'edit' === $form->getClickedButton()->getName()) {
+            $reservationId = $form->get('reservationId')->getData();
+            $roomReservations = $entityManager->getRepository(RoomReservation::class)->find($reservationId);
+
+            // Set status of approval to pending (-1)
+            $roomReservations->setApproved(-1);
+
+            // Set data elements
+            $roomReservations->setDate($form->get('date')->getData());
+            $roomReservations->setStartTime($form->get('startTime')->getData()->format('Hi'));
+            $roomReservations->setEndTime($form->get('endTime')->getData()->format('Hi'));
+            $roomReservations->setUserId($form->get('user')->getData()->getId());
+            $roomReservations->setRoomId($form->get('room')->getData()->getId());
+
+
+            // Handler if no template is used
+            if($form->get('reservationTemplate')->getData() == '') {
+                $roomReservations->setRoomDefaultReservationId(-1);
+                $entityManager->persist($roomReservations);
+                $entityManager->flush();
+            } else {
+                // Get the reservation template
+                $roomReservationTemplate = $entityManager->getRepository(RoomDefaultReservation::class)->findOneById($form->get('reservationTemplate')->getData());
+                $roomReservations->setRoomDefaultReservationId($form->get('reservationTemplate')->getData());
+
+                // Check if everything is in the correct time slot and room
+                if($roomReservationTemplate->getStartTime()       <= $form->get('startTime')->getData()->format('Hi') &&
+                    $roomReservationTemplate->getEndTime()    >= $form->get('endTime')->getData()->format('Hi') &&
+                    $roomReservationTemplate->getRoomId()       == $form->get('room')->getData()->getId()) {
+
+                    $roomReservations->setApproved(10);
+                    $entityManager->persist($roomReservations);
+                    $entityManager->flush();
+
+                } else { // Timeslot and/or room need approval
+                    if ($roomReservationTemplate->getRoomId() == $form->get('room')->getData()->getId()) {
+                        // Check and create a request for times before the template slot
+                        if ($roomReservationTemplate->getStartTime() > $form->get('startTime')->getData()->format('Hi')) {
+                            $roomReservationBefore = new RoomReservation();
+                            $roomReservationBefore->setApproved(-1);
+                            $roomReservationBefore->setDate($form->get('date')->getData());
+                            $roomReservationBefore->setStartTime($form->get('startTime')->getData()->format('Hi'));
+                            $roomReservationBefore->setEndTime($roomReservationTemplate->getStartTime());
+                            $roomReservationBefore->setUserId($form->get('user')->getData()->getId());
+                            $roomReservationBefore->setRoomId($form->get('room')->getData()->getId());
+                            $roomReservationBefore->setRoomDefaultReservationId(
+                                $form->get('reservationTemplate')->getData()
+                            );
+                            $entityManager->persist($roomReservationBefore);
+                            $entityManager->flush();
+                            $roomReservations->setStartTime($roomReservationTemplate->getStartTime());
+                        }
+                        // Check and create a request for times after the template slot
+                        if ($roomReservationTemplate->getEndTime() < $form->get('endTime')->getData()->format('Hi')) {
+                            $roomReservationAfter = new RoomReservation();
+                            $roomReservationAfter->setApproved(-1);
+                            $roomReservationAfter->setDate($form->get('date')->getData());
+                            $roomReservationAfter->setStartTime($roomReservationTemplate->getEndTime());
+                            $roomReservationAfter->setEndTime($form->get('endTime')->getData()->format('Hi'));
+                            $roomReservationAfter->setUserId($form->get('user')->getData()->getId());
+                            $roomReservationAfter->setRoomId($form->get('room')->getData()->getId());
+                            $roomReservationAfter->setRoomDefaultReservationId(
+                                $form->get('reservationTemplate')->getData()
+                            );
+                            $entityManager->persist($roomReservationAfter);
+                            $entityManager->flush();
+                            $roomReservations->setEndTime($roomReservationTemplate->getEndTime());
+                        }
+                        // Safe the reservation with the approved times
+                        $roomReservations->setApproved(10);
+                        $entityManager->persist($roomReservations);
+                        $entityManager->flush();
+                    } else { // Room has changed, so has to bee approved, no time check needed
+                        $roomReservations->setApproved(-1);
+                        $entityManager->persist($roomReservations);
+                        $entityManager->flush();
+                    }
+                }
+            }
+        }
+
         // Change date formating
         $date_now = date("d.m.Y");
         $date_displayend = date("d.m.Y", strtotime("+6 weeks", strtotime($date_now)));
